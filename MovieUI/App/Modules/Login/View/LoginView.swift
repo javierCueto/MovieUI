2caf5b1c117adc8a936cfb5afae0b1db9d29ad1c
Javier Cueto <39851501+javierCueto@users.noreply.github.com>
2022-08-11 16:52:19 -0600

    Login loader

--- MovieUI/App/Modules/Login/View/LoginView.swift
+++ MovieUI/App/Modules/Login/View/LoginView.swift
@@ -10,46 +10,80 @@ import SwiftUI
 struct LoginView: View {
     @State var email: String = ""
     @State var password: String = ""
+    @State var showingModal: Bool = false
     
     var body: some View {
-        
-        VStack {
-            Spacer()
-            
-            Text("Movie UI")
-                .foregroundColor(.red)
-                .font(.title)
-            Group {
-                TextField("Email", text: $email)
-                    .frame(maxHeight: 40)
-                    .padding(.horizontal)
-                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
-                    .padding(.horizontal)
+        ZStack {
+            if $showingModal.wrappedValue {
+                ZStack {
+                    Color.black.opacity(0.5)
+   
+                        .ignoresSafeArea()
+                    // This VStack is the popup
+                    ProgressView("Loading ...")
+                        .frame(width: 200, height: 100)
+                        .background(Color.white)
+                        .cornerRadius(20).shadow(radius: 20)
+                }
+                    .zIndex(2)
                 
-                SecureField("Password", text: $password)
-                    .frame(maxHeight: 40)
-                    .padding(.horizontal)
-                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
-                    .padding(.horizontal)
-                
-                Button("Save") {
-                    print("logged")
-                }.frame(maxWidth: .infinity, maxHeight: 40)
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
             }
-            Spacer()
-        }.background(
-            Image("wallpaper")
-                .resizable()
-                .aspectRatio(contentMode: ContentMode.fill)
-                .blur(radius: 100)
-                .ignoresSafeArea()
-        )
+            
+            VStack {
+                Spacer()
+                Text("Movie UI")
+                    .foregroundColor(.red)
+                    .font(.title)
+                Group {
+                    ZStack(alignment: .leading) {
+                        if email.isEmpty {
+                            Text("Email")
+                                .foregroundColor(.black)
+                                .opacity(0.4)
+                        }
+                        TextField("", text: $email)
+                    }
+                    .frame(maxHeight: 40)
+                    .padding(.horizontal)
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                    .padding(.horizontal)
+                    
+                    ZStack(alignment: .leading) {
+                        if password.isEmpty {
+                            Text("Password")
+                                .foregroundColor(.black)
+                                .opacity(0.4)
+                        }
+                        SecureField("Password", text: $password)
+                    }
+                    .frame(maxHeight: 40)
+                    .padding(.horizontal)
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.black, lineWidth: 1))
+                    .padding(.horizontal)
+                    
+                    Button("Save") {
+                        print("logged")
+                        showingModal.toggle()
+                    }.frame(maxWidth: .infinity, maxHeight: 40)
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+                
+                
+                Spacer()
+            }.background(
+                Image("wallpaper")
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fill)
+                    .blur(radius: 100)
+                    .ignoresSafeArea()
+            )
+            
+        }
         
-        
+       
     }
 }
 
